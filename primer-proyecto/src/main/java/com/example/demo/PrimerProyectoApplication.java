993c1fc12728bac732e82999199be44ef50f4938
alpheta25 <116258535+alpheta25@users.noreply.github.com>
2022-11-23 12:24:48 +0100

    añado ficheros de spring boot

---
+++ primer-proyecto/src/main/java/com/example/demo/PrimerProyectoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
+
+@SpringBootApplication
+public class PrimerProyectoApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+
+		
+	}
+
+}
